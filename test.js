361bc945c013cea3623b2a05cf69ff39c1cc319b
xiaowei <996196542@qq.com>
2021-09-19 16:11:20 +0800

    sass

--- test.js
+++ test.js
@@ -10,10 +10,5 @@
 // const as = 323;
 // const ass = 3232;
 // const s = 21;
-// const a = 100;
-// const b = 12323;
-// const c = 32323;
-// function df() {
-//   console.log(323);
-// }
-// const as = 21;
+
+const a = 32323;
