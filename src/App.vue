5152f1390ad5c262c8bbeb9b2b15c1fca1940b07
xiaowei <996196542@qq.com>
2021-09-19 15:55:52 +0800

    test4

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-console.log(3232)
+const as=3243;
+console.log(323);
 const a = 2020;
 import HelloWorld from './components/HelloWorld.vue';
 export default defineComponent({
